8f5a8f2de1a32dc2bcb7f2e48d0903b2f97639ba
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-10 08:43:24 -0600

    feat: Se ha creado la clase Proveedor en donde se llamaron y crearon los atributos de la entidad Proveedores

---
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/entity/Proveedor.java
@@ -0,0 +1,69 @@
+package com.marcosmontenegro.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Proveedores")
+
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_proveedor")
+    private Integer idProveedor;
+
+    @Column(name = "nombre_proveedor")
+    private String nombreEmpleado;
+
+    @Column(name = "telefono_proveedor")
+    private Integer telefonoProveedor;
+
+    @Column(name = "direccion")
+    private String direccionProveedor;
+
+    @Column(name = "email_proveedor")
+    private String emailProveedor;
+
+    // GETTERS AND SETTERS
+
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public Integer getTelefonoProveedor() {
+        return telefonoProveedor;
+    }
+
+    public void setTelefonoProveedor(Integer telefonoProveedor) {
+        this.telefonoProveedor = telefonoProveedor;
+    }
+
+    public String getDireccionProveedor() {
+        return direccionProveedor;
+    }
+
+    public void setDireccionProveedor(String direccionProveedor) {
+        this.direccionProveedor = direccionProveedor;
+    }
+
+    public String getEmailProveedor() {
+        return emailProveedor;
+    }
+
+    public void setEmailProveedor(String emailProveedor) {
+        this.emailProveedor = emailProveedor;
+    }
+}
