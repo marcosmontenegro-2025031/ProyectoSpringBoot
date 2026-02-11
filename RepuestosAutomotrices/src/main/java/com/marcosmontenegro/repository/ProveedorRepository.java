9ae4e49b6043adc0d1d503c82f9ae5f9898f92a0
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-11 10:50:39 -0600

    fix: Se hicieron cambios en ProveedorRepository

--- RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/ProveedorRepository.java
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/ProveedorRepository.java
@@ -2,7 +2,9 @@ package com.marcosmontenegro.repository;
 
 import com.marcosmontenegro.entity.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProveedorRepository extends JpaRepository<Proveedor, Integer> {
 
     boolean existsByNombreProveedorAndTelefonoProveedorAndDireccionProveedorAndEmailProveedor(
