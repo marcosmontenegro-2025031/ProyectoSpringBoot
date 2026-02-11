a31e5ef6c18b57c80faf0a666a599ca7bf98062c
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-11 10:04:14 -0600

    feat: Se ha creado la clase RepuestoRepository con exito

--- RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/RepuestoRepository.java
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/RepuestoRepository.java
@@ -1,4 +1,17 @@
 package com.marcosmontenegro.repository;
 
-public interface RepuestoRepository {
+import com.marcosmontenegro.entity.Proveedor;
+import com.marcosmontenegro.entity.Repuesto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
+
+    boolean existsNombreRepuestoAndCategoriaRepuestoAndPrecioCompraAndPrecioVentaAndProveedor(
+
+            String nombreRepuesto,
+            String categoriaRepuesto,
+            double precioCompra,
+            double precioVenta,
+            Proveedor proveedor
+    );
 }
