aba0d6eeda5319e4faeeb3623bed0cdad348f4bd
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-11 10:03:13 -0600

    feat: Se ha creado la clase Repuesto en donde se llamaron y crearon los atributos de la entidad Repuestos

---
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/entity/Repuesto.java
@@ -0,0 +1,81 @@
+package com.marcosmontenegro.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Repuestos")
+
+public class Repuesto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_repuesto")
+    private Integer idRepuesto;
+
+    @Column(name = "nombre_repuesto")
+    private String nombreRepuesto;
+
+    @Column(name = "categoria_repuesto")
+    private String categoriaRepuesto;
+
+    @Column(name = "precio_compra")
+    private double precioCompra;
+
+    @Column(name = "precio_venta")
+    private double precioVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_proveedor")
+    private Proveedor proveedor;
+
+    // GETTERS AND SETTERS
+
+
+    public Integer getIdRepuesto() {
+        return idRepuesto;
+    }
+
+    public void setIdRepuesto(Integer idRepuesto) {
+        this.idRepuesto = idRepuesto;
+    }
+
+    public String getNombreRepuesto() {
+        return nombreRepuesto;
+    }
+
+    public void setNombreRepuesto(String nombreRepuesto) {
+        this.nombreRepuesto = nombreRepuesto;
+    }
+
+    public String getCategoriaRepuesto() {
+        return categoriaRepuesto;
+    }
+
+    public void setCategoriaRepuesto(String categoriaRepuesto) {
+        this.categoriaRepuesto = categoriaRepuesto;
+    }
+
+    public double getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(double precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+}
---
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/RepuestoRepository.java
@@ -0,0 +1,4 @@
+package com.marcosmontenegro.repository;
+
+public interface RepuestoRepository {
+}
