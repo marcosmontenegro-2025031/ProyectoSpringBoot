c1fec536268d0e0052a1fd4aa0c662c43c3fde34
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-13 17:35:05 -0600

    fix: Se realizaron cambios en VentasRepository

--- RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/VentaRepository.java
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/VentaRepository.java
@@ -1,6 +1,9 @@
 package com.marcosmontenegro.repository;
 
 import com.marcosmontenegro.entity.Venta;
+
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface VentaRepository extends JpaRepository<Venta, Integer> {
 
     boolean existsByFechaVentaAndCantidadAndTotalAndIdEmpleadoAndIdRepuesto(
-            String fechaVenta,
-            Integer cantidad,
-            Double total,
-            Integer idEmpleado,
-            Integer idRepuesto
-    );
+        Date fechaVenta,
+        Integer cantidad,
+        Double total, 
+        Integer idEmpleado,
+        Integer idRepuesto);
 }
