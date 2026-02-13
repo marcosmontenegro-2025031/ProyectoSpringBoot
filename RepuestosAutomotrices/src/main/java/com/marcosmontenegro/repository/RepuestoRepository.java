756455113604253861db33e9d6cc11072f0b9910
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-13 00:20:41 -0600

    fix: Se realizaron cambios en RepuestosRepository

--- RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/RepuestoRepository.java
+++ RepuestosAutomotrices/src/main/java/com/marcosmontenegro/repository/RepuestoRepository.java
@@ -1,19 +1,18 @@
 package com.marcosmontenegro.repository;
 
-import com.marcosmontenegro.entity.Proveedor;
-import com.marcosmontenegro.entity.Repuesto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.marcosmontenegro.entity.Repuesto;
 
 @Repository
 public interface RepuestoRepository extends JpaRepository<Repuesto, Integer> {
 
-    boolean existsByNombreRepuestoAndCategoriaRepuestoAndPrecioCompraAndPrecioVentaAndProveedor(
-
+    boolean existsByNombreRepuestoAndCategoriaRepuestoAndPrecioCompraAndPrecioVentaAndIdProveedor(
             String nombreRepuesto,
             String categoriaRepuesto,
             Double precioCompra,
             Double precioVenta,
-            Proveedor proveedor
+            Integer idProveedor
     );
 }
